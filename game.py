c9ba5179ca6b6ce03aaf5185b20fd33934a79c08
acevedog <acevedog@bc.edu>
2015-09-17 21:54:10 -0400

    refactor compare code between p1 and p2 functions

--- game.py
+++ game.py
@@ -1,10 +1,32 @@
 def player2_is_strongly_dominated(array, col):
     payload_compare = get_player2_payload_col(array, col)
-    payload_p2 = get_player2_payload_col(array, 1)
-    for i in range(len(payload_compare)):
-        if compare_to(payload_compare[i], payload_p2[i]) == -1:
-            return True
+    payload_p2 = get_player2_payload_col(array, col+1)
+    if compare_payload(payload_compare, payload_p2) == True:
+        return True
     return False
+
+def player1_is_strongly_dominated(array, row):
+    payload_compare = get_player1_payload_row(array, row)
+    payload_p1 = get_player1_payload_row(array, row+1)
+    if compare_payload(payload_compare, payload_p1) == True:
+        return True
+    return False
+
+def get_player2_payload_col(array, col):
+    payload_array = []
+    for i in range(len(array)):
+        payload = array[i][col]
+        payload_p2 = payload[1]
+        payload_array.append(payload_p2)
+    return payload_array
+
+def get_player1_payload_row(array, row):
+    payload_array = []
+    for i in range(len(array)):
+        payload = array[row][i]
+        payload_p1 = payload[0]
+        payload_array.append(payload_p1)
+    return payload_array
 
 def get_player2_payload(array):
     payload_array = []
@@ -13,13 +35,6 @@ def get_player2_payload(array):
             payload = array[i][j]
             payload_p2 = payload[1]
             payload_array.append(payload_p2)
-    return payload_array
-
-def get_player2_payload_col(array, col):
-    payload_array = []
-    for i in range(len(array)):
-        payload = array[i][col]
-        payload_array.append(payload[1])
     return payload_array
 
 def get_player1_payload(array):
@@ -31,19 +46,9 @@ def get_player1_payload(array):
             payload_array.append(payload_p1)
     return payload_array
 
-def get_player1_payload_row(array, row):
-    payload_array = []
-    for i in range(len(array)):
-        payload = array[row][i]
-        payload_p1 = payload[0]
-        payload_array.append(payload_p1)
-    return payload_array
-
-def player1_is_strongly_dominated(array, row):
-    payload_compare = get_player1_payload_row(array, row)
-    payload_p1 = get_player1_payload_row(array, 1)
-    for i in range(len(payload_compare)):
-        if compare_to(payload_compare[i], payload_p1[i]) == -1:
+def compare_payload(pay1, pay2):
+    for i in range(len(pay1)):
+        if compare_to(pay1[i], pay2[i]) == -1:
             return True
     return False
 
