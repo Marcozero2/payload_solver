e60eb9af623f76c6898e481e4052f109dc1b91b5
acevedog <acevedog@bc.edu>
2015-09-17 22:07:58 -0400

    add documentation for functions

--- game.py
+++ game.py
@@ -1,4 +1,13 @@
+"""
+    Author: George Acevedo
+    Date: 17 September 2015
+"""
 def player2_is_strongly_dominated(array, col):
+    """
+        Compares the col with each col in the array.
+        Returns True if the col is dominated by another strategy.
+        Returns False otherwise.
+    """
     payload_compare = get_player2_payload_col(array, col)
     payload_p2 = get_player2_payload_col(array, col+1)
     if compare_payload(payload_compare, payload_p2) == True:
@@ -6,6 +15,11 @@ def player2_is_strongly_dominated(array, col):
     return False
 
 def player1_is_strongly_dominated(array, row):
+    """
+        Compares the row with each row in the array.
+        Returns True if the row is dominated by another strategy.
+        Returns False otherwise.
+    """
     payload_compare = get_player1_payload_row(array, row)
     payload_p1 = get_player1_payload_row(array, row+1)
     if compare_payload(payload_compare, payload_p1) == True:
@@ -13,6 +27,9 @@ def player1_is_strongly_dominated(array, row):
     return False
 
 def get_player2_payload_col(array, col):
+    """
+        Returns the payload for p2 for a col.
+    """
     payload_array = []
     for i in range(len(array)):
         payload = array[i][col]
@@ -21,6 +38,9 @@ def get_player2_payload_col(array, col):
     return payload_array
 
 def get_player1_payload_row(array, row):
+    """
+        Returns the payload for p1 for a row.
+    """
     payload_array = []
     for i in range(len(array)):
         payload = array[row][i]
@@ -28,7 +48,10 @@ def get_player1_payload_row(array, row):
         payload_array.append(payload_p1)
     return payload_array
 
-def get_player2_payload(array):
+def get_player_payload(array):
+    """
+        Returns all of the payloads in a array for p2.
+    """
     payload_array = []
     for i in range(len(array)):
         for j in range(len(array)):
@@ -38,6 +61,9 @@ def get_player2_payload(array):
     return payload_array
 
 def get_player1_payload(array):
+    """
+        Returns all of the payloads in a array for p1.
+    """
     payload_array = []
     for i in range(len(array)):
         for j in range(len(array)):
@@ -47,12 +73,22 @@ def get_player1_payload(array):
     return payload_array
 
 def compare_payload(pay1, pay2):
+    """
+        Returns True if all of the values in the first array.
+        that correspond to the values in the second array is greater.
+        Returns False otherwise.
+    """
     for i in range(len(pay1)):
         if compare_to(pay1[i], pay2[i]) == -1:
             return True
     return False
 
 def compare_to(a, b):
+    """
+        Returns 1 if the first parameter is greater than the second parameter.
+        Returns -1 if the first parameter is less than the second parameter.
+        Returns 0 the parameters are equal.
+    """
     if a > b:
         return 1
     elif a < b:
