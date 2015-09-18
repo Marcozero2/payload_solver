4f640bd6dfd29b8b00c7778e6f8f9e7c69348640
acevedog <acevedog@bc.edu>
2015-09-18 19:29:56 -0400

    add feature to check more than 1 columns for dominance

--- game.py
+++ game.py
@@ -1,7 +1,34 @@
-"""
+""" 
     Author: George Acevedo
     Date: 17 September 2015
 """
+
+def player2_is_strongly_dominated_all(array, col):
+    """
+        Compares the col with each col in the array.
+        Returns True if the col is dominated by another strategy.
+        Returns False otherwise.
+    """
+    payload_compare = get_player2_payload_col(array, col)
+    for j in range(len(array[0])-1): #for each col in the first row
+        payload_p2 = get_player2_payload_col(array, j+1) #so go to the right most col of the given col. and the len is 3, so stop at col 2
+        if compare_payload(payload_compare, payload_p2) == True:
+            return True
+    return False
+
+def player1_is_strongly_dominated_all(array, row):
+    """
+        Compares the row with each row in the array.
+        Returns True if the row is dominated by another strategy.
+        Returns False otherwise.
+    """
+    payload_compare = get_player1_payload_row(array, row)
+    for i in range(len(array)-1): #for each row in the first row
+        payload_p1 = get_player1_payload_row(array, i+1) #so go to the right most row of the given col. and the len is 3, so stop at row 2
+        if compare_payload(payload_compare, payload_p1) == True:
+            return True
+    return False
+
 def player2_is_strongly_dominated(array, col):
     """
         Compares the col with each col in the array.
@@ -48,7 +75,7 @@ def get_player1_payload_row(array, row):
         payload_array.append(payload_p1)
     return payload_array
 
-def get_player_payload(array):
+def get_player2_payload(array):
     """
         Returns all of the payloads in a array for p2.
     """
@@ -78,10 +105,14 @@ def compare_payload(pay1, pay2):
         that correspond to the values in the second array is greater.
         Returns False otherwise.
     """
+    count = 0
     for i in range(len(pay1)):
         if compare_to(pay1[i], pay2[i]) == -1:
-            return True
-    return False
+            count += 1
+    if count >= len(pay1):
+        return True
+    else:
+        return False
 
 def compare_to(a, b):
     """
--- game_test.py
+++ game_test.py
@@ -2,45 +2,115 @@ import unittest
 from game import *
 
 class TestPlayer2(unittest.TestCase):
+
+    """
+        Test for all of player1's and player 2's payloads
+    """
+    def test_get_player1_payload(self):
+        array = [[(0,0),(-1,-2)],[(-4,-1),(1,1)]]
+        expected_val = [0,-1,-4,1]
+        self.assertEqual(get_player1_payload(array), expected_val)
+
+        array = [[(0,1),(2,3),(1,0)], [(2,3),(1,1),(3,0)], [(1,1),(5,3),(2,0)]]
+        expected_val = [0, 2, 1, 2, 1, 3, 1, 5, 2]
+        self.assertEqual(get_player1_payload(array), expected_val)
+        
     def test_get_player_2_payload(self):
         array = [[(0,0),(-1,-3)],[(-3,-1),(1,1)]]
         expected_val = [0,-3,-1,1]
         self.assertEqual(get_player2_payload(array), expected_val)
 
-    def test_get_player_2_payload_col(self):
-        array = [[(0,0),(-1,-3)],[(-3,-1),(1,1)]]
-        expected_val = [0,-1]
-        self.assertEqual(get_player2_payload_col(array, 0), expected_val)
-        expected_val = [0,-2]
-        self.assertNotEqual(get_player2_payload_col(array, 0), expected_val)
+        array = [[(0,1),(2,3),(1,0)], [(2,3),(1,1),(3,0)], [(1,1),(5,3),(2,0)]]
+        expected_val = [1, 3, 0, 3, 1, 0, 1, 3, 0]
+        self.assertEqual(get_player2_payload(array), expected_val)
 
-    def test_player2_is_strongly_dominated(self):
-        array = [[(0,0),(-1,-3)],[(-3,-1),(1,1)]]
-        self.assertTrue(player2_is_strongly_dominated(array, 0))
-        array = [[(0,5),(-1,-3)],[(-3,6),(1,1)]]
-        self.assertFalse(player2_is_strongly_dominated(array, 0))
-        array = [[(0,5),(-1,10)],[(-3,6),(1,1)]]
-        self.assertTrue(player2_is_strongly_dominated(array, 0))
-
-    def test_get_player1_payload(self):
-        array = [[(0,0),(-1,-2)],[(-4,-1),(1,1)]]
-        expected_val = [0,-1,-4,1]
-        self.assertEqual(get_player1_payload(array), expected_val)
+    """
+        Test for player1's and player'2 payloads for a row or col
+    """
 
     def test_get_player1_payload_row(self):
         array = [[(0,0),(-1,-2)],[(-4,-1),(1,1)]]
         expected_val = [0,-1]
         self.assertEqual(get_player1_payload_row(array, 0), expected_val)
 
+        expected_val = [0,-2]
+        self.assertNotEqual(get_player1_payload_row(array, 0), expected_val)
+
+        array = [[(0,1),(2,3),(1,0)], [(2,3),(1,1),(3,0)], [(1,1),(5,3),(2,0)]]
+        expected_val = [0, 2, 1]
+        self.assertEqual(get_player1_payload_row(array, 0), expected_val)
+
+        expected_val = [1, 5, 2]
+        self.assertEqual(get_player1_payload_row(array, 2), expected_val)
+
+
+    def test_get_player2_payload_col(self):
+        array = [[(0,0),(-1,-3)],[(-3,-1),(1,1)]]
+        expected_val = [0,-1]
+        self.assertEqual(get_player2_payload_col(array, 0), expected_val)
+        
+        expected_val = [0,-2]
+        self.assertNotEqual(get_player2_payload_col(array, 0), expected_val)
+
+        array = [[(0,1),(2,3),(1,0)], [(2,3),(1,1),(3,0)], [(1,1),(5,3),(2,0)]]
+        expected_val = [1, 3, 1]
+        self.assertEqual(get_player2_payload_col(array, 0), expected_val)
+
+        array = [[(0,1),(2,3),(1,0)], [(2,3),(1,1),(3,0)], [(1,1),(5,3),(2,0)]]
+        expected_val = [0, 0, 0]
+        self.assertEqual(get_player2_payload_col(array, 2), expected_val)
+        
+
+    """
+        Test for a row or col is dominated by player1's or player's 2 other strategies, respectively
+    """
     def test_player1_is_strongly_dominated(self):
         array = [[(-8,0),(-6,-2)],[(-4,-1),(1,1)]]
         self.assertTrue(player1_is_strongly_dominated(array, 0))
+        
         array = [[(8,0),(6,-2)],[(4,-1),(1,1)]]
         self.assertFalse(player1_is_strongly_dominated(array, 0))
 
+    def test_player2_is_strongly_dominated(self):
+        array = [[(0,0),(-1,-3)],[(-3,-1),(1,1)]]
+        self.assertFalse(player2_is_strongly_dominated(array, 0))
+        
+        array = [[(0,5),(-1,-3)],[(-3,6),(1,1)]]
+        self.assertFalse(player2_is_strongly_dominated(array, 0))
+        
+        array = [[(0,5),(-1,10)],[(-3,6),(1,1)]]
+        self.assertFalse(player2_is_strongly_dominated(array, 0))
+
+        array = [[(0,0),(-1,20)],[(-3,0),(1,20)]]
+        self.assertTrue(player2_is_strongly_dominated(array, 0))
+    """
+        Test for all
+    """
+                        
+    def test_player2_is_strongly_dominated_all(self):
+        array = [[(0,1),(2,3),(1,0)], [(2,3),(1,1),(3,0)], [(1,1),(5,3),(2,0)]]
+        self.assertFalse(player2_is_strongly_dominated_all(array, 0))
+
+        array = [[(0,1),(2,3),(1,20)], [(2,3),(1,1),(3,20)], [(1,1),(5,3),(2,20)]]
+        self.assertTrue(player2_is_strongly_dominated_all(array, 0))
+    
+    """
+        Helper functions test(s)
+    """
+    
     def test_compare_to(self):
         self.assertEqual(compare_to(4, 1), 1)
         self.assertEqual(compare_to(1, 4), -1)
         self.assertEqual(compare_to(1, 1), 0)
+
+    def test_player1_is_strongly_dominated_all(self):
+        array = [[(0,1),(2,3),(1,20)], [(2,3),(1,1),(3,20)], [(1,1),(5,3),(2,20)]]
+        self.assertTrue(player1_is_strongly_dominated_all(array, 0))
+
+        array = [[(1,4),(5,3),(6,2)],[(8,2),(9,5),(10,11)],[(7,2),(23,8),(14,32)]]
+        self.assertTrue(player1_is_strongly_dominated_all(array, 0))
+
+        array = [[(50,4),(5,3),(6,2)],[(8,2),(9,5),(10,11)],[(7,2),(23,8),(14,32)]]
+        self.assertFalse(player1_is_strongly_dominated_all(array, 0))
         
 unittest.main(exit = False)
