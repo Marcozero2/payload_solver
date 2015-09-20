576515021e83dfa98f33a2ff2a8a8a8bf0b6eedf
acevedog <acevedog@bc.edu>
2015-09-20 15:05:57 -0400

    complete ruby port

--- game_port.rb
+++ game_port.rb
@@ -1,36 +1,134 @@
 #!/usr/bin/ruby
 
+=begin
+def player1_is_strongly_dominated_all_loop(array, row):
+    """
+        Compares the col with each col in the array.
+        Returns True if the col is dominated by another strategy.
+        Returns False otherwise.
+    """
+    payload_compare = get_player1_payload_row(array, row)
+    for i in range(row, len(array)-1): #for each row in the first row
+        payload_p1 = get_player1_payload_row(array, i+1) #so go to the right most row of the given col. and the len is 3, so stop at row 2
+        if compare_payload(payload_compare, payload_p1) == True:
+            return True
+    if row >= 1: #want to check the columns before the col
+        for j in range(0, row): #for each col in the first row
+            payload_p1 = get_player1_payload_row(array, j) #start at 0
+            if compare_payload(payload_compare, payload_p1) == True:
+                return True
+    return False
+=end
+
+def player1_is_strongly_dominated_all_loop(array, row)
+	payload_compare = get_player1_payload_row(array, row)
+	i = row
+	if row < array.length - 1
+		begin
+			payload_p1 = get_player1_payload_row(array, i+1)
+			if compare_payload(payload_compare, payload_p1) == true
+				return true
+			end
+			i += 1
+		end while i < array.length - 1
+	end
+	if row >= 1
+		j = 0
+		begin
+			payload_p1 = get_player1_payload_row(array, j)
+			if compare_payload(payload_compare, payload_p1) == true
+				return true
+			end
+			j += 1
+		end	 while j < row
+	end
+	return false
+end
+
+def player2_is_strongly_dominated(array, col)
+	payload_compare = get_player2_payload_col(array, col)
+	payload_p2 = get_player2_payload_col(array, col + 	1)
+	if compare_payload(payload_compare, payload_p2) == true
+		return true
+	end
+	return false
+end
+
+def player1_is_strongly_dominated(array, row)
+	payload_compare = get_player1_payload_row(array, row)
+	payload_p1 = get_player1_payload_row(array, row + 1)
+	if compare_payload(payload_compare, payload_p1) == true
+		return true
+	end
+	return false
+end
+
+def get_player2_payload_col(array, col)
+	payload_array = []
+	i = 0
+	begin 
+		payload = array[i][col]
+		payload_p2 = payload[1]
+		payload_array << payload_p2
+		i += 1
+	end while i < array.length
+	payload_array
+end
+
+def get_player1_payload_row(array, row)
+	payload_array = Array.new
+	i = 0
+	begin 
+		payload = array[row][i]
+		payload_p1 = payload[0]
+		payload_array << payload_p1
+		i += 1
+	end while i < array.length
+	payload_array
+end
+
 def get_player1_payload(array)
   payload_array = Array.new
   i = 0
   j = 0
   begin 
     begin 
-	  payload = array[i][j]
-	  puts "payload = #{payload}"
-	  payload_p1 = payload[0]
-	  payload_array << payload_p1
-	  j +=1
-	  puts "j= #{j}"
-	end while j < array[i].length
-	i += 1
-	j = 0
-	puts "i = #{i}"
+			payload = array[i][j]
+			payload_p1 = payload[0]
+			payload_array << payload_p1
+			j +=1
+		end while j < array[i].length
+		i += 1
+		j = 0
   end while i < array.length
   payload_array
+end
+        
+def get_player2_payload(array)
+	payload_array = Array.new
+	j = 0
+	i = 0
+	begin
+		begin
+			payload = array[i][j]
+			payload_p2 = payload[1]
+			payload_array << payload_p2
+			j += 1
+		end while j < array[i].length
+		i += 1
+		j = 0
+	end while i < array.length
+	payload_array
 end
 
 def compare_payload(pay1, pay2)
   count = 0
   i = 0
   begin
-    puts "payload1 = #{pay1[i]}"
-	puts "payload2 = #{pay2[i]}"
-	if compare_to(pay1[i], pay2[i]) == -1
-	  count += 1
-	end
-	i += 1
-	puts "i = #{i}"
+		if compare_to(pay1[i], pay2[i]) == -1
+			count += 1
+		end
+		i += 1
   end while i < pay1.length
   if count >= pay1.length
     return true
@@ -46,12 +144,3 @@ def compare_to(a, b)
   end
   return 0
 end
-
-array = [[[1,3],[5,10],[11,14]],[[14,21],[5,0],[13,4]],[[6,7],[9,10],[44,1]]]
-puts "array = #{array}"
-puts compare_to(20,20)
-puts compare_payload([10,10,10,10],[0,1,2,3])
-pay = Array.new(get_player1_payload(array))
-p pay
-
-
--- game_test_port.rb
+++ game_test_port.rb
@@ -4,16 +4,93 @@ require_relative"game_port"
 require "test/unit"
 
 class TestGamePort < Test::Unit::TestCase
+
+  def test_player1_is_strongly_dominated_all_loop
+    ary = [[[5,1],[3,1],[5,1]],[[4,1],[2,1],[0,1]],[[4,1],[3,1],[4,1]]]
+    assert_equal(true, player1_is_strongly_dominated_all_loop(ary, 1))
+    
+    ary = [[[0,1],[3,1],[5,1]],[[4,1],[2,1],[0,1]],[[4,1],[3,1],[4,1]]]
+    assert_equal(false, player1_is_strongly_dominated_all_loop(ary, 1))
+
+		ary = [[[0,1],[3,1],[5,1]],[[4,1],[2,1],[0,1]],[[-1,1],[-1,1],[-1,1]]]
+    assert_equal(true, player1_is_strongly_dominated_all_loop(ary, 2))
+		
+		ary = [[[0,1],[3,1],[5,1]],[[4,1],[2,1],[0,1]],[[4,1],[3,1],[4,1]]]
+		assert_equal(false, player1_is_strongly_dominated_all_loop(ary, 2))
+  end
+
+	def test_player2_is_strongly_dominated
+		ary = [[[0,0],[-1,-3]],[[-3,-1],[1,1]]]
+		assert_equal(false, player2_is_strongly_dominated(ary, 0))
+		
+    ary = [[[0,5],[-1,10]],[[-3,6],[1,1]]]
+    assert_equal(false, player2_is_strongly_dominated(ary, 0))
+
+    ary = [[[0,0],[-1,20]],[[-3,0],[1,20]]]
+    assert_equal(true, player2_is_strongly_dominated(ary, 0))
+	end
+
+	def test_player1_is_strongly_dominated
+		ary = [[[-8,0],[-6,-2]],[[-4,-1],[1,1]]]
+		assert_equal(true, player1_is_strongly_dominated(ary, 0))
+		
+		ary = [[[8,0],[6,-2]],[[4,-1],[1,1]]]
+		assert_equal(false, player1_is_strongly_dominated(ary, 0))
+	end
+		
+		
+	def test_get_player2_payload_col
+		ary = [[[0,0],[-1,3]],[[-3,-1],[1,1]]]
+		val = [0,-1]
+		assert_equal(val, get_player2_payload_col(ary, 0))
+	end
+	
+	def test_get_player1_payload_row 
+		ary = [[[0,0],[-1,-2]],[[-4,-1],[1,1]]]
+		val = [0,-1]
+    assert_equal(val, get_player1_payload_row(ary, 0))
+    
+    val = [0,-2]
+    assert_not_equal(val, get_player1_payload_row(ary, 0))
+    
+    ary = [[[0, 1],[2, 3],[1, 0]],[[2, 3],[1, 1],[3, 0]],[[1, 1],[5, 3],[2, 0]]]
+    val = [0, 2, 1]
+    assert_equal(val, get_player1_payload_row(ary, 0))
+    
+    val = [1, 5, 2]
+    assert_equal(val, get_player1_payload_row(ary, 2))
+  end
+	
+	def test_get_player1_payload
+		ary = [[0, 0], [-1, -2]], [[-4, -1] ,[1, 1]]
+		val = [0, -1, -4, 1]
+		assert_equal(val, get_player1_payload(ary))
+		
+		ary = [[[0,1],[2,3],[1,0]], [[2,3],[1,1],[3,0]], [[1,1],[5,3],[2,0]]]
+		val = [0, 2, 1, 2, 1, 3, 1, 5, 2]
+		assert_equal(val, get_player1_payload(ary))
+	end
+	
+	def test_get_player2_payload
+		ary = [[0, 0], [-1, -2]], [[-4, -1] ,[1, 1]]
+		val = [0, -2, -1, 1]
+		assert_equal(val, get_player2_payload(ary))
+		
+		ary = [[[0,1],[2,3],[1,0]], [[2,3],[1,1],[3,0]], [[1,1],[5,3],[2,0]]]
+		val = [1, 3, 0, 3, 1, 0, 1, 3, 0]
+		assert_equal(val, get_player2_payload(ary))
+	end
+        
   def test_compare_to
-    assert_equal(0, compare_to(10, 10))
+		assert_equal(0, compare_to(10, 10))
     assert_equal(1, compare_to(20, 10))
-	assert_equal(-1, compare_to(0, 10))
+		assert_equal(-1, compare_to(0, 10))
   end
 	
   def test_compare_payload
     assert_equal(false, compare_payload([20,8,6,4,2],[9,8,7,6,5]))
     assert_equal(true, compare_payload([0,0,0,0,0],[9,8,7,6,5]))
-  end		
+  end
 end
 
 
