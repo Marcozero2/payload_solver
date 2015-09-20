c0f8636eec6b4e7d3d80da7827803f26b5f82c59
acevedog <acevedog@bc.edu>
2015-09-20 04:19:50 -0400

    add and correct python port and test

--- game_port.rb
+++ game_port.rb
@@ -1,14 +1,40 @@
 #!/usr/bin/ruby
 
+def get_player1_payload(array)
+	payload_array = Array.new
+	i = 0
+	j = 0
+	begin 
+		begin 
+			payload = array[i][j]
+			puts "payload = #{payload}"
+			payload_p1 = payload[0]
+			payload_array << payload_p1
+			j +=1
+			puts "j= #{j}"
+		end while j < array[i].length
+		i += 1
+		j = 0
+		puts "i = #{i}"
+	end while i < array.length
+	payload_array
+end
+
 def compare_payload(pay1, pay2)
 	count = 0
 	i = 0
 	begin
+		puts "payload1 = #{pay1[i]}"
+		puts "payload2 = #{pay2[i]}"
 		if compare_to(pay1[i], pay2[i]) == -1
-			return true
+			count += 1
 		end
 		i += 1
+		puts "i = #{i}"
 	end while i < pay1.length
+	if count >= pay1.length
+		return true
+	end
 	return false 
 end
 
@@ -21,7 +47,11 @@ def compare_to(a, b)
 	return 0
 end
 
+array = [[[1,3],[5,10],[11,14]],[[14,21],[5,0],[13,4]],[[6,7],[9,10],[44,1]]]
+puts "array = #{array}"
 puts compare_to(20,20)
-puts compare_payload([-4,-3,-2,-2],[0,1,2,3])
+puts compare_payload([10,10,10,10],[0,1,2,3])
+pay = Array.new(get_player1_payload(array))
+p pay
 
 
--- game_test_port.rb
+++ game_test_port.rb
@@ -1,1 +1,19 @@
+#!/usr/bin/ruby
+
+require_relative"game_port"
+require "test/unit"
+
+class TestGamePort < Test::Unit::TestCase
+	def test_compare_to
+		assert_equal(0, compare_to(10, 10))
+		assert_equal(1, compare_to(20, 10))
+		assert_equal(-1, compare_to(0, 10))
+	end
+	
+	def test_compare_payload
+		assert_equal(false, compare_payload([20,8,6,4,2],[9,8,7,6,5]))
+		assert_equal(true, compare_payload([0,0,0,0,0],[9,8,7,6,5]))
+	end		
+end
+
 
